70ef9c7f1b12c92219b999df40cc92b01ec806b4
Alan-galue <alan.galue@davinci.edu.ar>
2025-12-09 09:52:40 -0300

    agregamos la  url del backend

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,4 @@
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:8000';
+const API_URL = import.meta.env.VITE_API_URL || 'https://final-backend-app-hibridas.onrender.com/';
 
 const apiRequest = async (endpoint, options = {}) => {
     const token = localStorage.getItem('token');
